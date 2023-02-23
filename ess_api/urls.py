6f06244b257c9029695829630080bbe971f842e6
Najeeb Shams <44796050+Najeeb-Shams@users.noreply.github.com>
2023-02-23 21:54:35 +0100

    Delete ess_api directory

--- ess_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path ('hi/', views.hi, name="Hello"), 
-    path ('events/', views.events_view , name='events_view'),
-    
-]
--- ess_api/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from  . sparql import events_list
-
-def events_view(request):
-     return render(request, 'events.html',events_list)
-    
