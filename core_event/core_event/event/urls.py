e38a75c50e42a665a0336d51eadb546556368bee
Najeeb Shams <44796050+Najeeb-Shams@users.noreply.github.com>
2023-02-23 23:47:23 +0100

    Add files via upload

---
+++ core_event/core_event/event/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('rank_events/', views.rank_events),
+    path('event_result/', views.event_result),
+    path('load_data/', views.load_excel_data_to_db),
+]
